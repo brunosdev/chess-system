ea9e6ac15c13f6a119c1ba74c35cdde61a2c86a6
bruunodev <brunosanttos@hotmail.com.br>
2022-03-27 15:14:38 -0300

    Implementing possible moves of Rook

--- src/application/UI.java
+++ src/application/UI.java
@@ -59,7 +59,8 @@ public class UI {
 	private static void printPiece(ChessPiece piece) {
 		if (piece == null) {
 			System.out.print("-");
-		} else {
+		} 
+		else {
 			if (piece.getColor() == Color.WHITE) {
 				System.out.print(ANSI_WHITE + piece + ANSI_RESET);
 			} else {
